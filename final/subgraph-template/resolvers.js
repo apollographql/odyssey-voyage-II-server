1cef48280c3a81bc77ca82750677bc3010985f5a
Michelle Mabuyo <michelle@apollographql.com>
2022-01-28 10:30:42 -0500

    add subgraph-template to final folder starting point (#8)

---
+++ final/subgraph-template/resolvers.js
@@ -0,0 +1,8 @@
+const { AuthenticationError, ForbiddenError } = require('apollo-server');
+const authErrMessage = '*** you must be logged in ***';
+
+const resolvers = {
+  // TODO: fill in resolvers
+};
+
+module.exports = resolvers;
