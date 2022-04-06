ef41c36abd81d2ee873265118e335ee3fd4c6d5f
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2022-04-06 17:53:37 -0400

    lesson 9b: resolvers for entry points

--- final/subgraph-accounts/resolvers.js
+++ final/subgraph-accounts/resolvers.js
@@ -2,6 +2,40 @@ const { AuthenticationError, ForbiddenError } = require('apollo-server');
 const authErrMessage = '*** you must be logged in ***';
 
 const resolvers = {
+  Query: {
+    user: async (_, { id }, { dataSources }) => {
+      const user = await dataSources.accountsAPI.getUser(id);
+      if (!user) {
+        throw new Error('No user found for this Id');
+      }
+      return user;
+    },
+    me: async (_, __, { dataSources, userId }) => {
+      if (!userId) throw new AuthenticationError(authErrMessage);
+      const user = await dataSources.accountsAPI.getUser(userId);
+      return user;
+    },
+  },
+  Mutation: {
+    updateProfile: async (_, { updateProfileInput }, { dataSources, userId }) => {
+      if (!userId) throw new AuthenticationError(authErrMessage);
+      try {
+        const updatedUser = await dataSources.accountsAPI.updateUser({ userId, userInfo: updateProfileInput });
+        return {
+          code: 200,
+          success: true,
+          message: 'Profile successfully updated!',
+          user: updatedUser,
+        };
+      } catch (err) {
+        return {
+          code: 400,
+          success: false,
+          message: err.message,
+        };
+      }
+    },
+  },
   User: {
     __resolveType(user) {
       return user.role;
