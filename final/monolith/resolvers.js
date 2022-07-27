ddf4fb1b48abe3743e8521cda3034c073a66a602
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2022-07-27 08:58:32 -0400

    lesson 10b: resolvers for entry points

--- final/monolith/resolvers.js
+++ final/monolith/resolvers.js
@@ -3,18 +3,6 @@ const authErrMessage = '*** you must be logged in ***';
 
 const resolvers = {
   Query: {
-    user: async (_, { id }, { dataSources }) => {
-      const user = await dataSources.accountsAPI.getUser(id);
-      if (!user) {
-        throw new Error('No user found for this Id');
-      }
-      return user;
-    },
-    me: async (_, __, { dataSources, userId }) => {
-      if (!userId) throw new AuthenticationError(authErrMessage);
-      const user = await dataSources.accountsAPI.getUser(userId);
-      return user;
-    },
     searchListings: async (_, { criteria }, { dataSources }) => {
       const { numOfBeds, checkInDate, checkOutDate, page, limit, sortBy } = criteria;
       const listings = await dataSources.listingsAPI.getListings({ numOfBeds, page, limit, sortBy });
@@ -98,24 +86,6 @@ const resolvers = {
     },
   },
   Mutation: {
-    updateProfile: async (_, { updateProfileInput }, { dataSources, userId }) => {
-      if (!userId) throw new AuthenticationError(authErrMessage);
-      try {
-        const updatedUser = await dataSources.accountsAPI.updateUser({ userId, userInfo: updateProfileInput });
-        return {
-          code: 200,
-          success: true,
-          message: 'Profile successfully updated!',
-          user: updatedUser,
-        };
-      } catch (err) {
-        return {
-          code: 400,
-          success: false,
-          message: err.message,
-        };
-      }
-    },
     createBooking: async (_, { createBookingInput }, { dataSources, userId }) => {
       if (!userId) throw new AuthenticationError(authErrMessage);
 
