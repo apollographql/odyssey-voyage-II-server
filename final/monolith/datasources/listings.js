b72065a924c4eb6b3180ca21d0f498320e5a3606
Michelle Mabuyo <michelle@apollographql.com>
2023-04-05 14:06:57 -0400

    final code - pass cache to RESTDataSource

--- final/monolith/datasources/listings.js
+++ final/monolith/datasources/listings.js
@@ -1,10 +1,7 @@
 const { RESTDataSource } = require('@apollo/datasource-rest');
 
 class ListingsAPI extends RESTDataSource {
-  constructor() {
-    super();
-    this.baseURL = 'http://localhost:4010/';
-  }
+  baseURL = 'http://localhost:4010/';
 
   getListingsForUser(userId) {
     return this.get(`user/${userId}/listings`);
