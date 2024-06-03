dfedf770636379c9399df07624128fa2241d0522
Michelle Mabuyo <michelle@apollographql.com>
2024-06-03 16:05:10 -0400

    lesson 3 - monolith as a subgraph

--- monolith/index.js
+++ monolith/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { buildSubgraphSchema } = require("@apollo/subgraph");
 
 const { readFileSync } = require("fs");
 const axios = require("axios");
@@ -18,11 +19,13 @@ const PaymentsAPI = require("./datasources/payments");
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema: buildSubgraphSchema({
+      typeDefs,
+      resolvers,
+    }),
   });
 
-  const port = 4000;
+  const port = 4001;
 
   try {
     const { url } = await startStandaloneServer(server, {
