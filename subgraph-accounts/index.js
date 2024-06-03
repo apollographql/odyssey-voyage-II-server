a8e70d6ab90c8fea2789465bc0d5bb679fe35cbc
Michelle Mabuyo <michelle@apollographql.com>
2024-06-03 17:28:03 -0400

    lesson 8a - subgraph duplicate template

---
+++ subgraph-accounts/index.js
@@ -0,0 +1,62 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { buildSubgraphSchema } = require("@apollo/subgraph");
+
+const { readFileSync } = require("fs");
+const axios = require("axios");
+const gql = require("graphql-tag");
+
+const { AuthenticationError } = require("./utils/errors");
+
+const typeDefs = gql(readFileSync("./schema.graphql", { encoding: "utf-8" }));
+const resolvers = require("./resolvers");
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema: buildSubgraphSchema({
+      typeDefs,
+      resolvers,
+    }),
+  });
+
+  const port = 0; // TODO: change port number
+  const subgraphName = ""; // TODO: change to subgraph name
+
+  try {
+    const { url } = await startStandaloneServer(server, {
+      context: async ({ req }) => {
+        const token = req.headers.authorization || "";
+        const userId = token.split(" ")[1]; // get the user name after 'Bearer '
+
+        let userInfo = {};
+        if (userId) {
+          const { data } = await axios
+            .get(`http://localhost:4011/login/${userId}`)
+            .catch((error) => {
+              throw AuthenticationError();
+            });
+
+          userInfo = { userId: data.id, userRole: data.role };
+        }
+
+        const { cache } = server;
+
+        return {
+          ...userInfo,
+          dataSources: {
+            // TODO: add data sources here
+          },
+        };
+      },
+      listen: {
+        port,
+      },
+    });
+
+    console.log(`🚀 Subgraph ${subgraphName} running at ${url}`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+startApolloServer();
---
+++ subgraph-accounts/resolvers.js
@@ -0,0 +1,10 @@
+const { AuthenticationError, ForbiddenError } = require("./utils/errors");
+
+const resolvers = {
+  // TODO: fill in resolvers
+  Query: {
+    _todo: () => "TODO",
+  },
+};
+
+module.exports = resolvers;
